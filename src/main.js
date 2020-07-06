cd4c2e74b56e8fba04a6bd04f6a2b5b4ea32b4d2
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-07-06 01:12:57 +0000

    Add tailwindcss, tailwindui and inter font

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './assets/css/tailwind.css'
+
+import './assets/fonts/Inter/inter.css' 
+
 Vue.config.productionTip = false
 
 new Vue({
