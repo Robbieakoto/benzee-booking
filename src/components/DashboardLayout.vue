f5729302e5cab55b7302d93c2183d02951bb7cbb
Roberta Akoto <akotoro22@gmail.com>
2020-07-21 00:54:31 +0100

    Add queries to get bookings and rooms

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -98,12 +98,12 @@
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
             <button
-              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:text-white"
               @click="displayProfileOnPhone()"
 
             >
-              <!-- Menu open: "hidden", Menu closed: "block" -->
-              <svg class="block h-6 w-6" stroke="currentColor" fill="currentColor" viewBox="0 0 24 24">
+              <!--Menu icon -->
+              <svg class="block h-6 w-6" stroke="currentColor" viewBox="0 0 20 24">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
@@ -112,8 +112,8 @@
                   v-if="!showProfileOnPhone"
                 />
               </svg>
-              <!-- Menu open: "block", Menu closed: "hidden" -->
-              <svg class="block h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <!--Close icon -->
+              <svg class="block h-6" stroke="currentColor" viewBox="0 0 20 24">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,7 +1,12 @@
 import gql from 'graphql-tag'
 
 import getRoomTypes from './getRoomTypes.gql'
+import getRooms from './getRooms.gql'
 import getCurrentAcademicYear from "./getCurrentAcademicYear.gql"
+import getBookings from "./getBookings.gql"
+
 
 export const GET_ROOM_TYPES = gql`${getRoomTypes}`
+export const GET_ROOMS= gql `${getRooms}`
 export const GET_CURRENT_ACADEMIC_YEAR = gql`${getCurrentAcademicYear}`
+export const GET_BOOKINGS = gql`${getBookings}`
