741e04d86f7ebb3064cf391231925d12c00daa04
Roberta Akoto <akotoro22@gmail.com>
2020-07-21 01:19:44 +0100

    List all residents

--- src/views/Residents/ResidentsList.vue
+++ src/views/Residents/ResidentsList.vue
@@ -30,101 +30,29 @@
                 </tr>
               </thead>
               <tbody class="bg-white">
-                <tr>
+                <tr v-for="resident in residents" :key="resident.id">
                   <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                     <div class="flex items-center">
                       <div class="ml-4">
-                        <div class="text-sm leading-5 font-medium text-gray-900">Bernard Lane</div>
-                        <div class="text-sm leading-5 text-gray-500">1st Year - Lancaster University</div>
+                        <div class="text-sm leading-5 font-medium text-gray-900">{{resident.name}}</div>
+                        <div class="text-sm leading-5 text-gray-500">{{resident.level}} - {{resident.institution}}</div>
                       </div>
                     </div>
                   </td>
                   <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div class="text-sm leading-5 text-gray-900">BG1</div>
-                    <div class="text-sm leading-5 text-gray-500">9 months</div>
+                    <div v-if="resident.bookings[0].room" class="text-sm leading-5 text-gray-900">{{ resident.bookings[0].room.name }}</div>
+                    <div v-else class="text-sm leading-5 text-gray-900">N/A</div>
                   </td>
                   <td
                     class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500"
                   >
                     <div class="text-sm leading-5 text-gray-900">
-                      0548797248
+                      {{resident.telephone}}
                     </div>
                   </td>
                   <td
                     class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
                   >
-                    <span
-                      class="inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-200 text-green-800"
-                    >
-                      <router-link :to="{name: 'resident-details'}">
-                        <svg class="h-6 w-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                          <path
-                            fill-rule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </router-link>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div class="flex items-center">
-                      <div class="ml-4">
-                        <div class="text-sm leading-5 font-medium text-gray-900">Joshua Addai</div>
-                        <div
-                          class="text-sm leading-5 text-gray-500"
-                        >Foundation - Lancaster University</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div class="text-sm leading-5 text-gray-900">BG3</div>
-                    <div class="text-sm leading-5 text-gray-500">12 months</div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div class="text-sm leading-5 text-gray-900">
-                      0556565650
-                    </div>
-                  </td>
-                  <td
-                    class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
-                  >
-                    <span
-                      class="inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-200 text-green-800"
-                    >
-                      <router-link :to="{name: 'resident-details'}">
-                        <svg class="h-6 w-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                          <path
-                            fill-rule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </router-link>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-6 py-4 whitespace-no-wrap">
-                    <div class="flex items-center">
-                      <div class="ml-4">
-                        <div class="text-sm leading-5 font-medium text-gray-900">Bernard Lane</div>
-                        <div class="text-sm leading-5 text-gray-500">2nd year &ndash; Knutsford University</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-no-wrap">
-                    <div class="text-sm leading-5 text-gray-900">CF2</div>
-                    <div class="text-sm leading-5 text-gray-500">12 months</div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                    <div class="text-sm leading-5 text-gray-900">
-                      0246807737
-                    </div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                     <span
                       class="inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-200 text-green-800"
                     >
@@ -176,11 +104,17 @@
 
 <script>
 import DashboardLayout from "@/components/DashboardLayout.vue";
+import { GET_RESIDENTS } from '@/graphql/queries'
 
 export default {
   name: "RoomList",
   components: {
     DashboardLayout
+  },
+  apollo: {
+    residents: {
+      query: GET_RESIDENTS
+    }
   }
 };
 </script>
